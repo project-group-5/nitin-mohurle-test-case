b85ea8c7f213395591b2c4ccd9ae22338859fb18
Nitinmohurle <102518982+Nitinmohurle@users.noreply.github.com>
2022-04-18 14:52:38 +0530

    Add files via upload

---
+++ src/main/java/seat/UserApplication.java
@@ -0,0 +1,17 @@
+package seat;
+
+public class UserApplication {
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+
+}
+
+}
