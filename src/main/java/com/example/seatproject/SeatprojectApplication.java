b8e9cd2538e977e8b6a4e5b944ba274c0dafd1d2
Nitinmohurle <102518982+Nitinmohurle@users.noreply.github.com>
2022-04-18 14:30:29 +0530

    Add files via upload

---
+++ src/main/java/com/example/seatproject/SeatprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.seatproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatprojectApplication.class, args);
+	}
+
+}
